9da7e8696fa11a0dd42b38ce0f225ea9db773541
funnymutant <fmutant@mutantNote.(none)>
2015-02-28 19:12:18 +0300

    1st and 2nd unit ready

--- unit2/polygon-creation.js
+++ unit2/polygon-creation.js
@@ -24,12 +24,13 @@ function PolygonGeometry(sides) {
 		var x = Math.cos( angle );
 		var y = Math.sin( angle );
 
-		// YOUR CODE HERE
-		//Save the vertex location - fill in the code
-
+		geo.vertices.push( new THREE.Vector3( x, y, 0 ) );
 	}
-	// YOUR CODE HERE
-	// Write the code to generate minimum number of faces for the polygon.
+	
+	for ( var pt = 0; pt < sides - 2; pt++ )
+	{
+		geo.faces.push( new THREE.Face3( 0, pt + 1, pt + 2 ) );
+	}
 
 	// Return the geometry object
 	return geo;
